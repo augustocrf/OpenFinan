eb5415b577d20afe0cb3a04b8733ff0e650ba6e2
augustocrf <augustocrf@gmail.com>
2024-05-18 18:09:21 -0300

    Retirado a validacao anterior

--- src/OpenFinan.WebApi/DTos/Financiamento/IncluiFinanciamentoPost.cs
+++ src/OpenFinan.WebApi/DTos/Financiamento/IncluiFinanciamentoPost.cs
@@ -7,18 +7,14 @@ namespace OpenFinan.WebApi.Dtos.Financiamento;
 
 public class IncluiFinanciamentoPost
 {
-    public int cpf  { get; set; }
-    public int idtipofinanciamento { get; set; }
-
-    [Range(5,72, 
-        ErrorMessage = "O campo {0} tem que estar entre {1} e {2}.")]
-    public int quantidadeparcela { get; set; }
-    
-    [Range(1, 1000000,
-        ErrorMessage = "O campo {0} tem que estar entre {1} e {2}.")]
-    public double valorcredito { get; set; }
-    
     [Required]
-    [CustomDateRange]
+    public int cpf  { get; set; }
+    [Required]
+    public int idtipofinanciamento { get; set; }
+    [Required]
+    public int quantidadeparcela { get; set; }
+    [Required]
+    public double valorcredito { get; set; }
+    [Required]
     public DateTime dataprimeiraparcela { get; set; }
 }
